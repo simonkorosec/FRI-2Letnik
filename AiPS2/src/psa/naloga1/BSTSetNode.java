b0515a1288ab1aa47d0c7d7dd664c95bfe9db4cb
simonkorosec <simon.korosec1997@gmail.com>
2018-02-27 18:46:15 +0100

    aps2

--- AiPS2/src/psa/naloga1/BSTSetNode.java
+++ AiPS2/src/psa/naloga1/BSTSetNode.java
@@ -7,130 +7,240 @@ import java.util.List;
  * Implementation of the (unbalanced) Binary Search Tree set node.
  */
 public class BSTSetNode {
-	private static int counter;
-	private BSTSetNode left, right, parent;
-	private int key;
+    private static int counter;
+    private BSTSetNode left, right, parent;
+    private int key;
 
-	public BSTSetNode(BSTSetNode l, BSTSetNode r, BSTSetNode p,
-			int key) {
-		super();
-		this.left = l;
-		this.right = r;
-		this.parent = p;
-		this.key = key;
-	}
+    public BSTSetNode(BSTSetNode l, BSTSetNode r, BSTSetNode p, int key) {
+        super();
+        this.left = l;
+        this.right = r;
+        this.parent = p;
+        this.key = key;
+    }
 
-	public BSTSetNode getLeft() {
-		return left;
-	}
+    public BSTSetNode getLeft() {
+        return left;
+    }
 
-	public void setLeft(BSTSetNode left) {
-		this.left = left;
-	}
+    public void setLeft(BSTSetNode left) {
+        this.left = left;
+    }
 
-	public BSTSetNode getRight() {
-		return right;
-	}
+    public BSTSetNode getRight() {
+        return right;
+    }
 
-	public void setRight(BSTSetNode right) {
-		this.right = right;
-	}
+    public void setRight(BSTSetNode right) {
+        this.right = right;
+    }
 
-	public int getKey() {
-		return key;
-	}
+    public int getKey() {
+        return key;
+    }
 
-	public void setKey(int key) {
-		this.key = key;
-	}
+    public void setKey(int key) {
+        this.key = key;
+    }
 
-	public int compare(BSTSetNode node) {
-		counter++;
-		return node.key-this.key;
-	}
+    public int compare(BSTSetNode node) {
+        counter++;
+        return node.key - this.key;
+    }
 
-	public int getCounter() {
-		return counter;
-	}
+    public int getCounter() {
+        return counter;
+    }
 
-	public void resetCounter() {
-		counter = 0;
-	}
+    public void resetCounter() {
+        counter = 0;
+    }
 
-	/**
-	 * If the element doesn't exist yet, adds the given element to the subtree.
-	 * 
-	 * @param element Given key wrapped inside an empty BSTNode instance
-	 * @return true, if the element was added; false otherwise.
-	 */
-	public boolean add(BSTSetNode element) {
-		throw new UnsupportedOperationException("You need to implement this function!");
-	}
-	
-	/**
-	 * Finds and removes the element with the given key from the subtree.
-	 * 
-	 * @param element Given key wrapped inside an empty BSTNode instance
-	 * @return true, if the element was found and removed; false otherwise.
-	 */
-	public boolean remove(BSTSetNode element) {
-		throw new UnsupportedOperationException("You need to implement this function!");
-	}
+    /**
+     * If the element doesn't exist yet, adds the given element to the subtree.
+     *
+     * @param element Given key wrapped inside an empty BSTNode instance
+     * @return true, if the element was added; false otherwise.
+     */
+    public boolean add(BSTSetNode element) {
+        int comp = this.compare(element);
 
-	/**
-	 * Checks whether the element with the given key exists in the subtree.
-	 * 
-	 * @param element Query key wrapped inside an empty BSTNode instance
-	 * @return true, if the element is contained in the subtree; false otherwise.
-	 */
-	public boolean contains(BSTSetNode element) {
-		throw new UnsupportedOperationException("You need to implement this function!");
-	}
+        if (comp == 0) {
+            return false;
+        } else if (comp < 0) {
+            if (this.left == null) {
+                this.setLeft(element);
+                this.left.parent = this;
+                return true;
+            } else {
+                return this.left.add(element);
+            }
+        } else {
+            if (this.right == null) {
+                this.setRight(element);
+                this.right.parent = this;
+                return true;
+            } else {
+                return this.right.add(element);
+            }
+        }
+    }
 
-	/**
-	 * Finds the smallest element in the subtree.
-	 * This function is in some cases used during
-	 * the removal of the element.
-	 * 
-	 * @return Smallest element in the subtree
-	 */
-	public BSTSetNode findMin() {
-		throw new UnsupportedOperationException("You need to implement this function!");
-	}
-	
-	/**
-	 * Depth-first pre-order traversal of the BST.
-	 * 
-	 * @return List of elements stored in BST obtained by pre-order traversing the tree.
-	 */
-	List<Integer> traversePreOrder() {
-		throw new UnsupportedOperationException("You need to implement this function!");
-	}
+    /**
+     * Finds and removes the element with the given key from the subtree.
+     *
+     * @param element Given key wrapped inside an empty BSTNode instance
+     * @return true, if the element was found and removed; false otherwise.
+     */
+    public boolean remove(BSTSetNode element) {
+        int comp = this.compare(element);
 
-	/**
-	 * Depth-first in-order traversal of the BST.
-	 * 
-	 * @return List of elements stored in BST obtained by in-order traversing the tree.
-	 */
-	List<Integer> traverseInOrder() {
-		throw new UnsupportedOperationException("You need to implement this function!");
-	}
+        if (comp == 0) {
+            if (this.left == null && this.right == null) {
+                int c = this.compare(this.parent);
+                if (c < 0) {
+                    this.parent.setRight(null);
+                } else if (c > 0) {
+                    this.parent.setLeft(null);
+                }
+                this.parent = null;
+            } else if (this.right == null) {
+                this.left.parent = this.parent;
+                this.parent.setRight(this.getLeft());
+            } else {
+                BSTSetNode min = getRight().findMin();
+                min.parent.setLeft(null);
+                min.parent = null;
+                this.setKey(min.getKey());
+            }
+            return true;
 
-	/**
-	 * Depth-first post-order traversal of the BST.
-	 * 
-	 * @return List of elements stored in BST obtained by post-order traversing the tree.
-	 */
-	List<Integer> traversePostOrder() {
-		throw new UnsupportedOperationException("You need to implement this function!");
-	}
+        } else if (comp < 0) {
+            if (this.left == null) {
+                return false;
+            }
+            return this.left.remove(element);
+        } else {
+            if (this.right == null) {
+                return false;
+            }
+            return this.right.remove(element);
+        }
+    }
 
-	/**
-	 * Breadth-first (or level-order) traversal of the BST.
-	 * 
-	 * @return List of elements stored in BST obtained by breadth-first traversal of the tree.
-	 */
-	List<Integer> traverseLevelOrder() {
-		throw new UnsupportedOperationException("You need to implement this function!");
-	}
+    /**
+     * Checks whether the element with the given key exists in the subtree.
+     *
+     * @param element Query key wrapped inside an empty BSTNode instance
+     * @return true, if the element is contained in the subtree; false otherwise.
+     */
+    public boolean contains(BSTSetNode element) {
+        int comp = this.compare(element);
+
+        if (comp == 0) {
+            return true;
+        } else if (comp < 0) {
+            if (this.left != null) {
+                return this.left.contains(element);
+            }
+            return false;
+        } else {
+            if (this.right != null) {
+                return this.right.contains(element);
+            }
+            return false;
+        }
+
+    }
+
+    /**
+     * Finds the smallest element in the subtree.
+     * This function is in some cases used during
+     * the removal of the element.
+     *
+     * @return Smallest element in the subtree
+     */
+    public BSTSetNode findMin() {
+        if (this.left == null) {
+            return this;
+        }
+        return this.left.findMin();
+    }
+
+    /**
+     * Depth-first pre-order traversal of the BST.
+     *
+     * @return List of elements stored in BST obtained by pre-order traversing the tree.
+     */
+    List<Integer> traversePreOrder() {
+
+        List<Integer> list = new LinkedList<>();
+        list.add(this.getKey());
+        if (this.left != null) {
+            list.addAll(this.getLeft().traversePreOrder());
+        }
+        if (this.right != null) {
+            list.addAll(this.getRight().traversePreOrder());
+        }
+        return list;
+    }
+
+    /**
+     * Depth-first in-order traversal of the BST.
+     *
+     * @return List of elements stored in BST obtained by in-order traversing the tree.
+     */
+    List<Integer> traverseInOrder() {
+        List<Integer> list = new LinkedList<>();
+        if (this.left != null) {
+            list.addAll(this.getLeft().traverseInOrder());
+        }
+        list.add(this.getKey());
+        if (this.right != null) {
+            list.addAll(this.getRight().traverseInOrder());
+        }
+        return list;
+    }
+
+    /**
+     * Depth-first post-order traversal of the BST.
+     *
+     * @return List of elements stored in BST obtained by post-order traversing the tree.
+     */
+    List<Integer> traversePostOrder() {
+        List<Integer> list = new LinkedList<>();
+        if (this.left != null) {
+            list.addAll(this.getLeft().traversePostOrder());
+        }
+        if (this.right != null) {
+            list.addAll(this.getRight().traversePostOrder());
+        }
+        list.add(this.getKey());
+        return list;
+    }
+
+    /**
+     * Breadth-first (or level-order) traversal of the BST.
+     *
+     * @return List of elements stored in BST obtained by breadth-first traversal of the tree.
+     */
+    List<Integer> traverseLevelOrder() {
+        List<Integer> list = new LinkedList<>();
+        List<BSTSetNode> zaPregled = new LinkedList<>();
+        zaPregled.add(this);
+
+        while (!zaPregled.isEmpty()){
+            BSTSetNode el = zaPregled.remove(0);
+            list.add(el.getKey());
+
+            if (el.getLeft() != null) {
+                zaPregled.add(el.getLeft());
+            }
+            if (el.getRight() != null) {
+                zaPregled.add(el.getRight());
+            }
+        }
+
+        return list;
+    }
 }
