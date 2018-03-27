a53eef6f88b58f590182db1462b2a2de64d0c980
Simon Korošec <simon.korosec1997@gmail.com>
2018-03-27 15:54:43 +0200

    hhjb

---
+++ pins/src/compiler/seman/NameChecker.java
@@ -0,0 +1,115 @@
+package compiler.seman;
+
+import compiler.abstr.*;
+import compiler.abstr.tree.*;
+
+/**
+ * Preverjanje in razresevanje imen (razen imen komponent).
+ * 
+ * @author sliva
+ */
+public class NameChecker implements Visitor {
+
+    /*
+    * Rabimo še tabelo simbolov, puščice potegnemo s setNameDef()
+    * */
+
+
+    @Override
+    public void visit(AbsArrType acceptor) {
+
+    }
+
+    @Override
+    public void visit(AbsAtomConst acceptor) {
+
+    }
+
+    @Override
+    public void visit(AbsAtomType acceptor) {
+
+    }
+
+    @Override
+    public void visit(AbsBinExpr acceptor) {
+
+    }
+
+    @Override
+    public void visit(AbsDefs acceptor) {
+
+    }
+
+    @Override
+    public void visit(AbsExprs acceptor) {
+
+    }
+
+    @Override
+    public void visit(AbsFor acceptor) {
+
+    }
+
+    @Override
+    public void visit(AbsFunCall acceptor) {
+
+    }
+
+    @Override
+    public void visit(AbsFunDef acceptor) {
+
+    }
+
+    @Override
+    public void visit(AbsIfThen accpetor) {
+
+    }
+
+    @Override
+    public void visit(AbsIfThenElse accpetor) {
+
+    }
+
+    @Override
+    public void visit(AbsPar acceptor) {
+
+    }
+
+    @Override
+    public void visit(AbsTypeDef acceptor) {
+
+    }
+
+    @Override
+    public void visit(AbsTypeName acceptor) {
+
+    }
+
+    @Override
+    public void visit(AbsUnExpr acceptor) {
+
+    }
+
+    @Override
+    public void visit(AbsVarDef acceptor) {
+
+    }
+
+    @Override
+    public void visit(AbsVarName acceptor) {
+
+    }
+
+    @Override
+    public void visit(AbsWhere acceptor) {
+
+    }
+
+    @Override
+    public void visit(AbsWhile acceptor) {
+
+    }
+
+    // TODO
+
+}
