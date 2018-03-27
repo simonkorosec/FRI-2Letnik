3a596a8a2370b19ae1d296def3fb787a11553258
simonkorosec <simon.korosec1997@gmail.com>
2018-03-27 19:06:28 +0200

    uv

--- UV/DN2/src/sample/Controller.java
+++ UV/DN2/src/sample/Controller.java
@@ -1,5 +1,7 @@
 package sample;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
@@ -7,6 +9,7 @@ import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -16,6 +19,34 @@ public class Controller {
     public ImageView imageView;
     public TextField urlInputField;
     public ProgressBar progressBar;
+    public Label labelUsedStorage;
+    public TextArea inputArea;
+    public MenuItem exitButton;
+    public ListView attachmentsListView;
+
+    private SteganographicImage stgImage;
+    private int maxStored;
+    private int used;
+    private File openedFile = null;
+
+    private void errorWindow(String error) {
+        Alert alert = new Alert(Alert.AlertType.ERROR, error, ButtonType.OK);
+        alert.showAndWait();
+
+    }
+
+    public void exitProgram() {
+        if (stgImage != null) {
+            String msg = "Do you wish to exit? All unsaved progress will be lost!";
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, msg, ButtonType.YES, ButtonType.NO);
+            alert.showAndWait();
+
+            if (alert.getResult() == ButtonType.YES) {
+                Platform.exit();
+            }
+        }
+        Platform.exit();
+    }
 
     @FXML
     public void openFromFile() {
@@ -28,11 +59,18 @@ public class Controller {
         fileChooser.getExtensionFilters().addAll(extFilterAll, extFilterJPG, extFilterPNG);
 
         //Show open file dialog
-        File file = fileChooser.showSaveDialog(null);
-        if (file != null) {
-            loadImg(file);
+        openedFile = fileChooser.showOpenDialog(null);
+        try {
+            stgImage = SteganographicImage.loadFromFile(openedFile.getAbsolutePath());
+            loadImg(openedFile);
+            syncProgressBar();
+        } catch (IOException e) {
+            errorWindow("Sorry there was a problem with opening the image. :(");
+            return;
+        } catch (NullPointerException e) {
+            // Pass
         }
-        syncProgressBar();
+
     }
 
     private void loadImg(File file) {
@@ -40,21 +78,35 @@ public class Controller {
             loadImg(file.toURI().toURL().toExternalForm());
         } catch (MalformedURLException e) {
             e.printStackTrace();
+        } catch (NullPointerException e) {
+            // Pass
         }
     }
 
     private void loadImg(String path) {
-        double requestedSize = Main.stageWidth * 0.6;
-        imageView.setImage(new Image(path, requestedSize, requestedSize, true, false));
+        imageView.setImage(new Image(path));
+        maxStored = stgImage.getTotalStorage();
+        used = stgImage.getUsedStorage();
+
     }
 
     @FXML
     public void openFromURL() {
         String url = urlInputField.getText();
 
-        if(isValidURL(url)){
+        if (isValidURL(url)) {
+            try {
+                stgImage = SteganographicImage.loadFromUrl(url);
+            } catch (IOException e) {
+                errorWindow("Sorry couldn't access the given URL.");
+                return;
+            }
+
             loadImg(url);
-        } else{
+
+            syncProgressBar();
+
+        } else {
             errorWindow("Not a valid URL");
         }
     }
@@ -68,18 +120,150 @@ public class Controller {
         }
     }
 
-
     private void syncProgressBar() {
-        if (progressBar == null){
+        if (progressBar == null) {
             progressBar = new ProgressBar();
         }
 
-        progressBar.setProgress(100.0 / 200.0);
+        if (stgImage != null) {
+            used = stgImage.getUsedStorage();
+            double d = (double) used / (double) maxStored;
+            progressBar.setProgress(d);
+            labelUsedStorage.setText(String.format("%d / %d", used, maxStored));
+        }
     }
 
-    private void errorWindow(String error){
-        Alert alert = new Alert(Alert.AlertType.ERROR, error, ButtonType.OK);
-        alert.showAndWait();
+    @FXML
+    public void setMessage() {
+        if (stgImage != null) {
+            setMessage(inputArea.getText());
+        } else {
+            errorWindow("No image opened. Please open an image.");
+        }
+    }
 
+    @FXML
+    public void addMessage() {
+        if (stgImage != null) {
+            String msg = stgImage.getMessage();
+            if (!msg.equals("")) {
+                msg += "\n";
+            }
+
+            msg += inputArea.getText();
+            setMessage(msg);
+        } else {
+            errorWindow("No image opened. Please open an image.");
+        }
+    }
+
+    private void setMessage(String msg) {
+        try {
+            stgImage.setMessage(msg);
+            syncProgressBar();
+        } catch (SteganographicImage.SteganographicImageException e) {
+            errorWindow(e.getMessage());
+        }
+    }
+
+    @FXML
+    public void showMessage() {
+        if (stgImage != null) {
+            String msg = stgImage.getMessage();
+            Alert alert = new Alert(Alert.AlertType.NONE, msg, ButtonType.OK);
+            alert.showAndWait();
+
+        } else {
+            errorWindow("No image opened. Please open an image.");
+        }
+    }
+
+    @FXML
+    public void clearMessage() {
+        if (stgImage != null) {
+            setMessage("");
+            Alert alert = new Alert(Alert.AlertType.NONE, "Message cleared.", ButtonType.OK);
+            alert.showAndWait();
+
+        } else {
+            errorWindow("No image opened. Please open an image.");
+        }
+
+    }
+
+    private void save(String filename) {
+        try {
+            stgImage.saveToFile(filename);
+        } catch (IOException e) {
+            errorWindow("There was an error trying save the image.");
+        }
+    }
+
+    public void save() {
+        String name = openedFile.getName();
+        String ext = name.substring(name.lastIndexOf('.') + 1, openedFile.getName().length());
+
+        if (ext.toLowerCase().equals("png")) {
+            save(openedFile.getAbsolutePath());
+        } else {
+            saveAs();
+        }
+    }
+
+    public void saveAs() {
+        FileChooser fileChooser = new FileChooser();
+
+        //Set extension filter
+        FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG files", "*.png");
+        fileChooser.getExtensionFilters().addAll(extFilterPNG);
+
+        //Show save file dialog
+        File saveTo = fileChooser.showSaveDialog(null);
+
+        if (saveTo != null) {
+            save(saveTo.getAbsolutePath());
+        }
+
+    }
+
+    public void clearTextArea() {
+        inputArea.clear();
+    }
+
+
+    public void addAttachment() {
+        FileChooser fileChooser = new FileChooser();
+
+        //Set extension filter
+//        FileChooser.ExtensionFilter extFilterAll = new FileChooser.ExtensionFilter("All supported files", "*.jpg", "*.jpeg", "*.png");
+//        FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG/JPEG files", "*.jpg", "*.jpeg");
+//        FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG files", "*.png");
+//        fileChooser.getExtensionFilters().addAll(extFilterAll, extFilterJPG, extFilterPNG);
+
+        //Show open file dialog
+        File file = fileChooser.showOpenDialog(null);
+        try {
+            if (stgImage != null){
+                stgImage.addAttachment(file.getAbsolutePath());
+            } else {
+                errorWindow("Open an image before adding an attachment");
+            }
+
+        } catch (IOException e) {
+            errorWindow("Sorry there was a problem with opening the attachment.");
+        } catch (NullPointerException e) {
+            // Pass
+        } catch (SteganographicImage.SteganographicImageException e) {
+            errorWindow("Sorry there was a problem with adding the attachment.");
+        }
+
+    }
+
+    private void listAttachments() {
+        String[] attachments = stgImage.listAttachments();
+
+        for (String attachment : attachments) {
+            attachmentsListView.getItems().add(attachment);
+        }
     }
 }
--- UV/DN2/src/sample/Main.java
+++ UV/DN2/src/sample/Main.java
@@ -4,25 +4,43 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    static double stageWidth;
+    Stage stage;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        primaryStage.setOnCloseRequest(e -> {
+            e.consume();
+            close();
+        });
+
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
-        stageWidth = primaryStage.getWidth();
 
-        primaryStage.widthProperty().addListener(((observable, oldValue, newValue) -> stageWidth = primaryStage.getWidth()));
+        stage = primaryStage;
+    }
+
+    private void close() {
+        String msg = "Do you wish to exit? All unsaved progress will be lost!";
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, msg, ButtonType.YES, ButtonType.NO);
+        alert.showAndWait();
+
+        if (alert.getResult() == ButtonType.YES) {
+            stage.close();
+        }
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
