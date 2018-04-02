159d27d2729edde835d1cfe7fac01b9253fb1bf9
simonkorosec <simon.korosec1997@gmail.com>
2018-04-02 21:43:07 +0200

    aps

--- AiPS2/src/aps2/viterbi/PublicTests.java
+++ AiPS2/src/aps2/viterbi/PublicTests.java
@@ -1,8 +1,11 @@
 package aps2.viterbi;
 
 import aps2.viterbi.Viterbi;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
+
 import junit.framework.TestCase;
 
 /**
@@ -12,45 +15,61 @@ import junit.framework.TestCase;
  */
 
 public class PublicTests extends TestCase {
-	protected void setUp() throws Exception {
-	}
-	
-	public void testInitialMemoizationState() throws Exception {
-		double[][] probabilities = {{1.0, 0.0, 1.0, 0.0}};
-		
-		Viterbi v = new Viterbi(probabilities);
-		v.calculateOptimalPath();
-		
-		assertEquals(0.5, v.getMemoizationPriceMatrix()[0][0]);
-		assertEquals(-1, v.getMemoizationHistoryMatrix()[0][0]);
-	}
-	
-	public void testMemoizationMatrixSmall() throws Exception {
-		double[][] probabilities = {{0.2, 0.8, 1.0, 0.0}};
-		
-		Viterbi v = new Viterbi(probabilities);
-		v.calculateOptimalPath();
-		
-		double[][] memoizationPriceMatrix = {{0.5, 0.5},{0.5, 0.4}};
-		int[][] memoizationHistoryMatrix = {{-1, -1}, {1, 0}};
-		
-		assertTrue(Arrays.equals(memoizationPriceMatrix[0], v.getMemoizationPriceMatrix()[0]));
-		assertTrue(Arrays.equals(memoizationPriceMatrix[1], v.getMemoizationPriceMatrix()[1]));
-		assertTrue(Arrays.equals(memoizationHistoryMatrix[0], v.getMemoizationHistoryMatrix()[0]));
-		assertTrue(Arrays.equals(memoizationHistoryMatrix[1], v.getMemoizationHistoryMatrix()[1]));
-	}
-	
-	public void testOptimalPathSmall() throws Exception {
-		double[][] probabilities = {{0.2, 0.8, 1.0, 0.0}};
-		Integer[] optimalPath = {1, 0};
-		LinkedList<Integer> optPath = new LinkedList(Arrays.asList(optimalPath));
-		double optimalPathProbability = 0.5;
-		
-		Viterbi v = new Viterbi(probabilities);
-		v.calculateOptimalPath();
-		
-		assertEquals(optPath, v.getOptimalPath());
-		assertEquals(optimalPathProbability, v.getOptimalPathProbability());
-		assertEquals(optimalPath[optimalPath.length-1].intValue(), v.getOptimalResultingState());
-	}
+    protected void setUp() throws Exception {
+    }
+
+    public void testInitialMemoizationState() throws Exception {
+        double[][] probabilities = {{1.0, 0.0, 1.0, 0.0}};
+
+        Viterbi v = new Viterbi(probabilities);
+        v.calculateOptimalPath();
+
+        assertEquals(0.5, v.getMemoizationPriceMatrix()[0][0]);
+        assertEquals(-1, v.getMemoizationHistoryMatrix()[0][0]);
+    }
+
+    public void testMemoizationMatrixSmall() throws Exception {
+        double[][] probabilities = {{0.2, 0.8, 1.0, 0.0}};
+
+        Viterbi v = new Viterbi(probabilities);
+        v.calculateOptimalPath();
+
+        double[][] memoizationPriceMatrix = {{0.5, 0.5}, {0.5, 0.4}};
+        int[][] memoizationHistoryMatrix = {{-1, -1}, {1, 0}};
+
+        assertTrue(Arrays.equals(memoizationPriceMatrix[0], v.getMemoizationPriceMatrix()[0]));
+        assertTrue(Arrays.equals(memoizationPriceMatrix[1], v.getMemoizationPriceMatrix()[1]));
+        assertTrue(Arrays.equals(memoizationHistoryMatrix[0], v.getMemoizationHistoryMatrix()[0]));
+        assertTrue(Arrays.equals(memoizationHistoryMatrix[1], v.getMemoizationHistoryMatrix()[1]));
+    }
+
+    public void testOptimalPathSmall() throws Exception {
+        double[][] probabilities = {{0.2, 0.8, 1.0, 0.0}};
+        Integer[] optimalPath = {1, 0};
+        LinkedList<Integer> optPath = new LinkedList(Arrays.asList(optimalPath));
+        double optimalPathProbability = 0.5;
+
+        Viterbi v = new Viterbi(probabilities);
+        v.calculateOptimalPath();
+
+        assertEquals(optPath, v.getOptimalPath());
+        assertEquals(optimalPathProbability, v.getOptimalPathProbability());
+        assertEquals(optimalPath[optimalPath.length - 1].intValue(), v.getOptimalResultingState());
+    }
+
+//    public void testMAtrix() {
+//        double[][] probabilities = {{0.5, 0.5, 1.0, 0.0}, {0.3, 0.7, 0.75, 0.25}};
+//        Integer[] optimalPath = {1, 0, 1};
+//        LinkedList<Integer> optPath = new LinkedList(Arrays.asList(optimalPath));
+//        double optimalPathProbability = 0.5;
+//
+//        Viterbi v = new Viterbi(probabilities);
+//        v.calculateOptimalPath();
+//
+//        assertEquals(optPath, v.getOptimalPath());
+//        assertEquals(optimalPathProbability, v.getOptimalPathProbability());
+//        assertEquals(optimalPath[optimalPath.length - 1].intValue(), v.getOptimalResultingState());
+//    }
+
+
 }
--- AiPS2/src/aps2/viterbi/Viterbi.java
+++ AiPS2/src/aps2/viterbi/Viterbi.java
@@ -6,98 +6,171 @@ import java.util.List;
 
 /**
  * @author matevz
- *
  */
 public class Viterbi {
-	
-	public double probability[][];
-	int n; // number of states
-	int m; // number of nodes per state
-	
-	/**
-	 * @param probability Matrix of transition probabilities between consecutive
-	 * states of size n columns x m^2 rows, where n is the number of states and
-	 * m the number of nodes per each state.
-	 * 
-	 * Example for n=3 and m=2. Let there be nodes A1, A2 on 0th state, B1, B2
-	 * on 1st state, and C1, C2 on 2nd state with probabilities:
-	 * A1 -> B1: 0.2
-	 * A1 -> B2: 0.8
-	 * A2 -> B1: 0.0
-	 * A2 -> B2: 1.0
-	 * B1 -> C1: 0.4
-	 * B1 -> C2: 0.6
-	 * B2 -> C1: 0.3
-	 * B2 -> C2: 0.7
-	 * 
-	 * is encoded as:
-	 * probability[0][0] = 0.2;
-	 * probability[0][1] = 0.8;
-	 * probability[0][2] = 0.0;
-	 * probability[0][3] = 1.0;
-	 * probability[1][0] = 0.4;
-	 * probability[1][1] = 0.6;
-	 * probability[1][2] = 0.3;
-	 * probability[1][3] = 0.7;
-	 *
-	 * For details, consult page:
-	 * https://ucilnica.fri.uni-lj.si/mod/assign/view.php?id=13961
-	 */
-	public Viterbi(double probability[][]){
-		this.probability = probability;
-		this.n = probability.length;
-		this.m = (int)Math.sqrt(probability[0].length);
-		
-		// you can add your own initialization here
-	}
-	
-	/**
-	 * Calculates path from any initial state to any final state with highest
-	 * probability using dynamic programming and memoization.
-	 */
-	public final void calculateOptimalPath() {
-		throw new UnsupportedOperationException("You need to implement this function!");
-	}
-	
-	/**
-	 * @return Index of the optimal node in the last state.
-	 */
-	public int getOptimalResultingState() {
-		throw new UnsupportedOperationException("You need to implement this function!");
-	}
-	
-	/**
-	 * @return List of nodes in the optimal path.
-	 */
-	public List<Integer> getOptimalPath() {
-		throw new UnsupportedOperationException("You need to implement this function!");
-	}
-	
-	/**
-	 * @return Overall probability of the optimal path.
-	 */
-	public double getOptimalPathProbability() {
-		throw new UnsupportedOperationException("You need to implement this function!");
-	}
-	
-	/**
-	 * @return Intermediate memomization price matrix of size (n+1)*m generated
-	 * during dynamic programming calls. It contains best
-	 * probabilities for each state for specific node. Probabilities for the
-	 * initial state are equal to 1.0/m.
-	 */
-	public double[][] getMemoizationPriceMatrix() {
-		throw new UnsupportedOperationException("You need to implement this function!");
-	}
-	
-	/**
-	 * @return Intermediate memomization matrix of size (n+1)*m generated
-	 * during dynamic programming calls. For each state for specific node, it
-	 * contains the predecessor of the node on the optimal path from the initial
-	 * state to the final state. Predecessors of nodes in the first state are
-	 * set to -1.
-	 */
-	public int[][] getMemoizationHistoryMatrix() {
-		throw new UnsupportedOperationException("You need to implement this function!");
-	}
+
+    public double probability[][];
+    int n; // number of states
+    int m; // number of nodes per state
+
+    private LinkedList<Node> optPath;
+
+    /**
+     * @param probability Matrix of transition probabilities between consecutive
+     *                    states of size n columns x m^2 rows, where n is the number of states and
+     *                    m the number of nodes per each state.
+     *                    <p>
+     *                    Example for n=3 and m=2. Let there be nodes A1, A2 on 0th state, B1, B2
+     *                    on 1st state, and C1, C2 on 2nd state with probabilities:
+     *                    A1 -> B1: 0.2
+     *                    A1 -> B2: 0.8
+     *                    A2 -> B1: 0.0
+     *                    A2 -> B2: 1.0
+     *                    B1 -> C1: 0.4
+     *                    B1 -> C2: 0.6
+     *                    B2 -> C1: 0.3
+     *                    B2 -> C2: 0.7
+     *                    <p>
+     *                    is encoded as:
+     *                    probability[0][0] = 0.2;
+     *                    probability[0][1] = 0.8;
+     *                    probability[0][2] = 0.0;
+     *                    probability[0][3] = 1.0;
+     *                    probability[1][0] = 0.4;
+     *                    probability[1][1] = 0.6;
+     *                    probability[1][2] = 0.3;
+     *                    probability[1][3] = 0.7;
+     *                    <p>
+     *                    For details, consult page:
+     *                    https://ucilnica.fri.uni-lj.si/mod/assign/view.php?id=13961
+     */
+    public Viterbi(double probability[][]) {
+        this.probability = probability;
+        this.n = probability.length;
+        this.m = (int) Math.sqrt(probability[0].length);
+
+        // you can add your own initialization here
+        optPath = new LinkedList<>();
+    }
+
+    /**
+     * Calculates path from any initial state to any final state with highest
+     * probability using dynamic programming and memoization.
+     */
+    public final void calculateOptimalPath() {
+        int index = this.getOptimalResultingState();
+        int[][] history = this.getMemoizationHistoryMatrix();
+        double[][] price = this.getMemoizationPriceMatrix();
+
+        for (int level = history.length - 1; level >= 0; level--) {
+            optPath.addFirst(new Node(index, price[level][index]));
+            index = history[level][index];
+        }
+
+    }
+
+    /**
+     * @return Index of the optimal node in the last state.
+     */
+    public int getOptimalResultingState() {
+        double[][] priceMatrix = this.getMemoizationPriceMatrix();
+        int best = 0;
+
+        for (int i = 1; i < m; i++) {
+            if (priceMatrix[best][priceMatrix[best].length - 1] < priceMatrix[i][priceMatrix[i].length - 1]) {
+                best = i;
+            }
+        }
+
+        return best;
+    }
+
+    /**
+     * @return List of nodes in the optimal path.
+     */
+    public List<Integer> getOptimalPath() {
+        LinkedList<Integer> path = new LinkedList<>();
+        for (Node n : optPath) {
+            path.add(n.index);
+        }
+        return path;
+    }
+
+    /**
+     * @return Overall probability of the optimal path.
+     */
+    public double getOptimalPathProbability() {
+        return optPath.peekLast().probability;
+    }
+
+    /**
+     * @return Intermediate memomization price matrix of size (n+1)*m generated
+     * during dynamic programming calls. It contains best
+     * probabilities for each state for specific node. Probabilities for the
+     * initial state are equal to 1.0/m.
+     */
+    public double[][] getMemoizationPriceMatrix() {
+        double[][] priceMatrix = new double[n + 1][m];
+        for (int i = 0; i < priceMatrix[0].length; i++) {
+            priceMatrix[0][i] = 1.0 / m;
+        }
+
+        for (int i = 1; i < priceMatrix.length; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < m; k++) {
+                    int indexPovezave = m * j + k;
+                    double prejsnNode = priceMatrix[i - 1][j];
+                    double povezava = probability[i - 1][indexPovezave];
+                    double p = prejsnNode * povezava;
+                    priceMatrix[i][k] = Math.max(priceMatrix[i][k], p);
+                }
+            }
+        }
+
+        return priceMatrix;
+    }
+
+    /**
+     * @return Intermediate memomization matrix of size (n+1)*m generated
+     * during dynamic programming calls. For each state for specific node, it
+     * contains the predecessor of the node on the optimal path from the initial
+     * state to the final state. Predecessors of nodes in the first state are
+     * set to -1.
+     */
+    public int[][] getMemoizationHistoryMatrix() {
+        int[][] historyMatrix = new int[n + 1][m];
+        double[][] priceMatrix = new double[n + 1][m];
+
+        for (int i = 0; i < historyMatrix[0].length; i++) {
+            historyMatrix[0][i] = -1;
+            priceMatrix[0][i] = 1.0 / m;
+        }
+
+        for (int i = 1; i < priceMatrix.length; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < m; k++) {
+                    int indexPovezave = m * j + k;
+                    double prejsnNode = priceMatrix[i - 1][j];
+                    double povezava = probability[i - 1][indexPovezave];
+                    double p = prejsnNode * povezava;
+                    if (p > priceMatrix[i][k]) {
+                        priceMatrix[i][k] = p;
+                        historyMatrix[i][k] = j;
+                    }
+                }
+            }
+        }
+
+        return historyMatrix;
+    }
+
+    private class Node {
+        int index;
+        double probability;
+
+        public Node(int index, double probability) {
+            this.index = index;
+            this.probability = probability;
+        }
+    }
 }
