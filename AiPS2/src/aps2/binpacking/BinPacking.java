91f269fd8419c9efc1bfb2eacd97fe3618a1ff7a
simonkorosec <simon.korosec1997@gmail.com>
2018-06-01 19:27:10 +0200

    aps | uv

--- AiPS2/src/aps2/binpacking/BinPacking.java
+++ AiPS2/src/aps2/binpacking/BinPacking.java
@@ -1,44 +1,152 @@
 package aps2.binpacking;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Bin packing algorithm to compute the minimum number of bins to store all the
- * given items.
+ * * given items.
  */
 public class BinPacking {
-	int[] items; // sizes of items to store
-	int m; // size of each bin
+    int[] items; // sizes of items to store
+    int m; // size of each bin
 
-	public BinPacking(int[] items, int m) {
-		this.items = items;
-		this.m = m;
-	}
+    /* spremenljivke za exact */
+    private int[] bins;
+    private int[] razporeditev;
+    private int[] bestRazporeditev;
 
-	/**
-	 * Computes the strictly minimal number of bins for the given items.
-	 * 
-	 * @return Array of size n which stores the index of bin, where each item is stored in.
-	 */
-	public int[] binPackingExact() {
-		throw new UnsupportedOperationException("You need to implement this function!");
-	}
-	
-	/**
-	 * Uses heuristics (e.g. first-fit, best-fit, ordered first-fit etc.) to
-	 * compute the minimal number of bins for the given items. The smaller the
-	 * number, the more tests you will pass.
-	 * 
-	 * @return Array of size n which stores the index of bin, where each item is stored in.
-	 */
-	public int[] binPackingApproximate() {
-		throw new UnsupportedOperationException("You need to implement this function!");
-	}
+
+    public BinPacking(int[] items, int m) {
+        this.items = items;
+        this.m = m;
+
+        razporeditev = new int[items.length];
+        bestRazporeditev = new int[items.length];
+        int velikost = 0;
+        for (int item : items) {
+            velikost += item;
+        }
+        velikost = (int) Math.ceil(velikost / (m * 1.0));
+        bins = new int[velikost];
+    }
+
+    /**
+     * Computes the strictly minimal number of bins for the given items.
+     *
+     * @return Array of size n which stores the index of bin, where each item is stored in.
+     */
+    public int[] binPackingExact() {
+        for (int i = 0; i < razporeditev.length; i++) {
+            razporeditev[i] = -1;
+            bestRazporeditev[i] = i;
+        }
+
+        recursive(0);
+        return bestRazporeditev;
+    }
+
+    private void recursive(int itemID) {
+        if (itemID >= items.length) {
+            if (betterSolution()) {
+                bestRazporeditev = razporeditev.clone();
+            }
+            return;
+        }
+
+        for (int i = 0; i < bins.length; i++) {
+            putIntoBin(itemID, i);
+            recursive(itemID + 1);
+            removeFromBin(itemID, i);
+        }
+    }
+
+    private boolean betterSolution() {
+        for (int bin : bins) {
+            if (bin > m) {
+                return false;
+            }
+        }
+
+        HashSet<Integer> bestHS = new HashSet<>();
+        HashSet<Integer> currHS = new HashSet<>();
+
+        for (int i = 0; i < razporeditev.length; i++) {
+            currHS.add(razporeditev[i]);
+            bestHS.add(bestRazporeditev[i]);
+        }
+        int best = bestHS.size();
+        int curr = currHS.size();
+
+        return curr < best;
+    }
+
+    private void removeFromBin(int itemID, int i) {
+        razporeditev[itemID] = -1;
+        bins[i] -= items[itemID];
+    }
+
+    private void putIntoBin(int itemID, int i) {
+        razporeditev[itemID] = i;
+        bins[i] += items[itemID];
+    }
+
+    /**
+     * Uses heuristics (e.g. first-fit, best-fit, ordered first-fit etc.) to
+     * compute the minimal number of bins for the given items. The smaller the
+     * number, the more tests you will pass.
+     *
+     * @return Array of size n which stores the index of bin, where each item is stored in.
+     */
+    public int[] binPackingApproximate() {
+        int binNum = 0;
+        bins = new int[items.length];
+
+        int[] order = order();
+
+        for (int i : order) {
+            int velikost = items[i];
+
+            int j;
+            for (j = 0; j < binNum; j++) {
+                if (bins[j] >= velikost){
+                    bins[j] -= velikost;
+                    bestRazporeditev[i] = j;
+                    break;
+                }
+            }
+
+            if (j == binNum){
+                bins[binNum] = m - velikost;
+                bestRazporeditev[i] = binNum;
+                binNum++;
+            }
+        }
+
+        return bestRazporeditev;
+    }
+
+    private int[] order() {
+        TreeMap<Integer, Integer> order = new TreeMap<>();
+        for (int i = 0; i < items.length; i++) {
+            order.put(i, items[i]);
+        }
+
+        TreeMap<Integer, Integer> neworder = new TreeMap<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                int c = order.get(o2).compareTo(order.get(o1));
+                if (c == 0) return 1;
+                return c;
+            }
+        });
+        neworder.putAll(order);
+
+        int [] zap = new int[neworder.size()];
+        int j = 0;
+        for (int i : neworder.keySet()) {
+            zap[j++] = i;
+        }
+        return zap;
+    }
 
 }
--- UV/DN5/playLevel3.qml
+++ UV/DN5/playLevel3.qml
@@ -14,6 +14,7 @@ Rectangle {
     property string barvaPloscic: "red"
     property string barvaCrk: "white"
     property string beseda: "avto"
+    property var capitalization
 
     ColumnLayout {
         anchors.fill: parent
@@ -65,7 +66,6 @@ Rectangle {
 
                     Row {
                         id: crkeDestination
-                        property string name: "row"
                         property ListModel arr: ListModel {}
 
                         anchors.verticalCenter: parent.verticalCenter
@@ -96,7 +96,6 @@ Rectangle {
                     ListModel {
                         id: vnosModel
                     }
-
                 }
             }
 
@@ -126,6 +125,7 @@ Rectangle {
 
             Row {
                 id: crkeSource
+                property ListModel array: ListModel {}
 
                 //anchors.left: parent.left
                 anchors.verticalCenter: parent.verticalCenter
@@ -138,7 +138,8 @@ Rectangle {
 
                 Repeater {
                     model: crkeModel
-                    delegate: DragTile { colorKey: lvl.barvaPloscic
+                    delegate: DragTile {
+                        colorKey: lvl.barvaPloscic
                         colorText: lvl.barvaCrk
                         colorCorrect: lvl.colorCorrect
                         colorWrong: lvl.colorWrong
@@ -168,14 +169,25 @@ Rectangle {
 
                     var result = JSON.parse(settings.responseText);
                     lvl.barva = result["bgColor"];
-                    var cap = result["fontCapitalization"];
+                    capitalization = result["fontCapitalization"];
                     lvl.barvaPloscic = result["barvaPloscic"];
                     lvl.barvaCrk = result["barvaCrk"];
                     lvl.colorCorrect = result["colorCorrect"];
                     lvl.colorWrong = result["colorWrong"];
 
                     var iskanaBeseda = beseda;
-                    readWord(cap, iskanaBeseda);
+
+                    if (capitalization === "Font.AllUppercase"){
+                        capitalization = Font.AllUppercase;
+                        console.log("upper");
+                        console.log(Font.AllUppercase);
+                    } else {
+                        console.log("lower");
+                        console.log(Font.AllLowercase);
+                        capitalization = Font.AllLowercase;
+                    }
+
+                    readWord(iskanaBeseda, capitalization);
                 } else {
                     console.log("HTTP:", settings.status, settings.statusText);
                 }
@@ -184,12 +196,7 @@ Rectangle {
         settings.send();
     }
 
-    function readWord(capitalization, iskanaBeseda){
-        if (capitalization === "Font.AllUppercase"){
-            capitalization = Font.AllUppercase;
-        } else {
-            capitalization = Font.AllLowercase;
-        }
+    function readWord(iskanaBeseda, capit){
 
         var request = new XMLHttpRequest();
         request.open('GET', './json/words.json');
@@ -202,11 +209,10 @@ Rectangle {
                     for (var i in list) {
                         if (list[i].tekst === iskanaBeseda.toLowerCase()){
                             pomocTekst.text = "";
-//                            pomocTekst.font.capitalization = capitalization;
 
                             slikaImg.source = list[i].slika;
 
-                            pomesajCrke(iskanaBeseda);
+                            pomesajCrke(iskanaBeseda, capit);
                             return;
                         }
                     }
@@ -218,12 +224,12 @@ Rectangle {
         request.send();
     }
 
-    function pomesajCrke(beseda) {
+    function pomesajCrke(beseda, capit) {
         var a = beseda.split("");
         var n = a.length;
 
         for(var c in a){
-            vnosModel.append({"crka" : a[c], "indeks": c});
+            vnosModel.append({"crka" : a[c], "indeks": c, "capitalization":capitalization});
         }
 
         for(var i = n - 1; i > 0; i--) {
@@ -236,7 +242,7 @@ Rectangle {
         beseda = a.join("");
 
         for(c in a){
-            crkeModel.append({"crka" : a[c]});
+            crkeModel.append({"crka" : a[c], "capitalization":capitalization});
         }
     }
 
